2d81c7f5f5d076558501908ea9e55dbdec1a6a97
Victor Romero <vromerocomino@gmail.com>
2021-06-16 16:39:10 +0200

    fix: node v14

--- part5-node-utils/nivell3-1.js
+++ part5-node-utils/nivell3-1.js
@@ -2,7 +2,7 @@
 // Crea una funció que creï dos fitxers codificats en hexadecimal i en base64 
 // respectivament, a partir del fitxer de l'exercici inicial.
 
-const fsPromises = require('fs').promises
+const fsPromises = require('fs/promises')
 const convertTo = async (path, output, encoding = 'utf8') => {
   const content = await fsPromises.readFile(path)
   const newContent = content.toString(encoding)
