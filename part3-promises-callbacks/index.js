fade5e703a17a6df5da4779197c3e41149932eb4
Víctor Romero <vromerocomino@gmail.com>
2021-07-06 10:05:05 +0200

    part3: N1-E2 updated. Different message depending on input

--- part3-promises-callbacks/index.js
+++ part3-promises-callbacks/index.js
@@ -2,22 +2,26 @@
 // Exercici 1
 function promiseGen (bool) {
   return new Promise((res, rej) => {
-    bool ? res('Promesa resolguda correctament') : rej('Promesa rebutjada')  
+    bool ? res('N1-E1: Promesa resolguda correctament') : rej('N1-E1: Promesa rebutjada')  
   })
 }
 
 promiseGen(5 > 3)
-  .then(res => console.log(res)) // output: 'Promesa resolguda correctament'
+  .then(res => console.log(res)) // output: 'N1-E1: Promesa resolguda correctament'
   .catch(err => console.log(err))
 
 promiseGen(3 > 5)
   .then(res => console.log(res))
-  .catch(err => console.log(err)) // output: 'Promesa rebutjada'
+  .catch(err => console.log(err)) // output: 'N1-E1: Promesa rebutjada'
 
 // Exercici 2
 const printer = (str) => console.log(str)
-const printMessage = (str, fn) => fn(str)
-printMessage('Hello World', printer) // output: 'Hello World'
+const canIDrink = (age, fn) => {
+  const message = age >= 18 ? `N1-E2: Tens ${age} anys. Pots beure` : `N1-E2: Et falten ${18-age} anys per poder beure`
+  fn(message)
+}
+canIDrink(23, printer) // output: 'N1-E2: Tens 23 anys.Pots beure'
+canIDrink(13, printer) // output: 'N1-E2: Et falten 5 anys per poder beure'
 
 /* Nivell 2 */
 // Exercici 1
@@ -46,17 +50,17 @@ let salaries = [{
 const getEmployee = (id) => {
   const employee = employees.find(e => e.id === id)
   return new Promise((res, rej) => {
-    employee ? res(employee) : rej(`Employee with id ${id} not found`)
+    employee ? res(employee) : rej(`N2-E1: Employee with id ${id} not found`)
   })
 }
 
 getEmployee(2)
-  .then(res => console.log(res))
-  .catch(err => console.log(err)) // output: { id: 2, name: 'Bill Gates' }
+  .then(res => console.log('N2-E1: ' + JSON.stringify(res))) // output: 'N2-E1: { id: 2, name: 'Bill Gates' }'
+  .catch(err => console.log(err)) 
 
 getEmployee(10)
   .then(res => console.log(res))
-  .catch(err => console.log(err)) // output: Employee with id 10 not foun
+  .catch(err => console.log(err)) // output: 'N2-E1: Employee with id 10 not found'
 
 // Exercici 2
 const getSalario = (employee) => {
@@ -67,21 +71,21 @@ const getSalario = (employee) => {
 }
 
 getSalario({id: 2, name: 'Bill Gates'})
-  .then(res => console.log(res)) // output: El salari del treballador 2 és: 1000
+  .then(res => console.log('N2-E2: ' + res)) // output: 'N2-E2: El salari del treballador 2 és: 1000'
   .catch(err => console.log(err))
 
 getSalario({id: 10, name: 'Vic'})
   .then(res => console.log(res))
-  .catch(err => console.log(err)) // output: Salary of employee with id 10 not found
+  .catch(err => console.log('N2-E2: ' + err)) // output: 'N2-E2: Salary of employee with id 10 not found'
 
 // Exercici 3
 getEmployee(3)
   .then(res => getSalario(res))
-  .then(res => console.log(res)) // output: El salari del treballador 3 és: 2000
+  .then(res => console.log('N2-E3: ' + res)) // output: 'El salari del treballador 3 és: 2000'
 
 /* Nivell 3*/ 
 // Exercici 1
 getEmployee(8)
   .then(res => getSalario(res))
   .then(res => console.log(res))
-  .catch(err => console.log(err)) // output: Employee with id 8 not found
+  .catch(err => console.log('N3-E1: ' + err)) // output: Employee with id 8 not found
