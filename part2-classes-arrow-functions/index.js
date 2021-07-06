b9a75ac29bc7271d39101fd0151283f36a8788fd
Víctor Romero <vromerocomino@gmail.com>
2021-07-06 09:51:59 +0200

    part2: fix N3-E1 using abstract class

--- part2-classes-arrow-functions/index.js
+++ part2-classes-arrow-functions/index.js
@@ -1,7 +1,7 @@
 /* Nivell 1 */
 // Exercici 1
-const twoNames = ((a, b) => a + b)(5, 27)
-console.log(twoNames) // output: 32
+const sum = ((a, b) => a + b)(5, 27)
+console.log(`N1-E1: ${sum}`) // output: 'N1-E1: 32'
 
 /* Nivell 2 */
 // Exercici 1
@@ -10,30 +10,55 @@ const objectCreator = (value) => {
     name: value
   }
 }
-console.log(objectCreator('Víctor Romero')) // output: {name: 'Vic Romero'}
+console.log(objectCreator('Víctor Romero')) // output: {name: 'Víctor Romero'}
 
 // Exercici 2
 class Persona {
-  constructor(nom) {
-    this.nom = nom
+  constructor(nombre) {
+    this.nombre = nombre
   }
   decirNombre () {
-    console.log(this.nom)
+    console.log(`N2-E2: ${this.nombre}`)
   }
 }
 const victor = new Persona('Víctor Romero')
-victor.decirNombre() // output: 'Víctor Romero'
+victor.decirNombre() // output: 'N2-E2: Víctor Romero'
 
 /* Nivell 3 */
 // Exercici 1
-function Person (name) {
-  this.name = name
-  this.sayName = () => {
-    console.log(this.name)
+// Clase abstracta
+class Person {
+  constructor () {
+    if (this.constructor === Person) {
+      throw new Error ("Person is an abstract class and can't be instantiated.")
+    }
+  }
+  sayName () {
+    console.log(`N3-E1: My name is ${this.name}`)
+  }
+  sayRole () {
+    throw new Error ("Method 'sayRole()' has to be implemented.")
   }
 }
-const vic = new Person('Víctor Romero')
-const dan = new Person('Dan Abramov')
 
-vic.sayName() // output: 'Víctor Romero'
-dan.sayName() // output: 'Dan Abramov'
+// Funció que genera objectes a partir de la clase abstracta
+function DevStudent (name, role) {
+  this.name = name
+  this.role = role
+}
+
+// Herencia
+DevStudent.prototype = Object.create(Person.prototype)
+
+// Implementació del métode sayRole()
+DevStudent.prototype.sayRole = function () {
+  console.log(`N3-E1: My role is ${this.role}`)
+}
+
+const vic = new DevStudent('Víctor Romero', 'NodeJs Dev')
+const dan = new DevStudent('Dan Abramov', 'ReactJs Dev')
+
+vic.sayName() // output: 'N3-E1: My name is Víctor Romero'
+vic.sayRole() // output: 'N3-E1: My role is NodeJs Dev'
+dan.sayName() // output: 'N3-E1: My name is Dan Abramov'
+dan.sayRole() // output: 'N3-E1: My role is ReactJs Dev'
